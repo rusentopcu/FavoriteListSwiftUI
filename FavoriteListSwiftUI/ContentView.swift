192a257546b87c17dde21bf84ce73c4825649f79
Rusen Topcu <rusentopcu@msn.com>
2020-04-27 17:18:04 +0300

    Project create

---
+++ FavoriteListSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FavoriteListSwiftUI
+//
+//  Created by Rusen Topcu on 27.04.2020.
+//  Copyright © 2020 Rusen Topcu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
